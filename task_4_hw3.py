264ba8599b08dadf49d272a28de42cc0430813f6
Zrierby <124761442+Zrierby@users.noreply.github.com>
2023-05-08 18:24:40 +1000

    Add files via upload

---
+++ task_4_hw3.py
@@ -0,0 +1,17 @@
+import os
+from pathlib import Path
+from typing import Optional, Callable
+
+
+def universal_file_counter(dir_path: Path, file_extension: str, tokenizer: Optional[Callable] = None) -> int:
+    counter = 0
+    for file in dir_path.glob(f'*.{file_extension}'):
+        with file.open('r') as f:
+            if tokenizer is None:
+                counter += len(f.readlines())
+            else:
+                counter += len(tokenizer(' '.join(f.readlines())))
+    return counter
+
+
+print(universal_file_counter(Path('src/'), "txt", str.split))
